f64e3d5fb6107531539e374f914d6a02bf3954e8
Koledloda <matveikoledin070306@gmail.com>
2024-05-20 09:34:17 +0300

    Добавьте файлы проекта.

---
+++ Prctos2/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace prctos2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
